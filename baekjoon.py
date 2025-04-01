6cf3f0c29637cc7b655770af2e99d2efe061be8b
tpqls774 <tpqls774@dgsw.hs.kr>
2025-04-01 23:26:05 +0900

    baekjoon 30802

---
+++ baekjoon.py
@@ -0,0 +1,16 @@
+n = int(input())
+size = list(map(int, input().split()))
+t, p = map(int, input().split())
+min_b = 0
+
+for i in size:
+    if i == 0:
+        continue
+    if t >= i:
+        min_b += 1
+    else:
+        min_b += (i + t - 1) // t
+
+
+print(min_b)
+print(n//p, n%p)
